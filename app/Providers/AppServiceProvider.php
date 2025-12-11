70cc75a3742cb07fefc5a4bfd4f02bd2e43f3733
Samuel Pacheco <samuelpachecopst@gmail.com>
2025-12-11 14:33:53 -0300

    start

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Number;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        if ($this->app->environment('local')) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (app()->environment(['production', 'staging'])) {
+            URL::forceScheme('https');
+        }
+
+        // Model::shouldBeStrict();
+
+        Number::useLocale('pt-BR');
+    }
+}
